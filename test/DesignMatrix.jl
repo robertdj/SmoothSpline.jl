1ce1ca062d72a2e5f1621641900704c7c4808876
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-09-16 20:55:58 +0200

    🧪 Failing test with Gram matrix comparison

--- test/DesignMatrix.jl
+++ test/DesignMatrix.jl
@@ -4,14 +4,25 @@ using Test
 # import RCall
 using RCall
 import RDatasets
+import QuadGK
 import LinearAlgebra
 
+import Random
+
 include("ReshapeROutput.jl")
+# include("ExampleSpline.jl")
 
 @testset "Design matrix" begin
     mtcars = RDatasets.dataset("datasets", "mtcars")
     hp = mtcars[!, :HP]
     mpg = mtcars[!, :MPG]
+
+    Random.seed!(1)
+    hp = [0.0; 1/3; 0.5; 1.0]
+    # hp = [0.0; rand(4); 1.0] |> sort
+    mpg = rand(length(hp))
+    # hp = 1:4
+    # mpg = 1:4
     
     spline_model_r = RCall.rcopy(R"smooth.spline($hp, $mpg, spar = 0.5, keep.stuff = TRUE)")
     
@@ -39,7 +50,7 @@ include("ReshapeROutput.jl")
         # W = LinearAlgebra.diagm(spline_data.W)
         # scaled_observations_julia = transpose(design_matrix_julia) * W * spline_data.Y
     
-        scaled_observations_julia = compute_tikhonov_matrix(spline_data, 0.5)[2]
+        _, scaled_observations_julia = compute_tikhonov_matrix(spline_data, 0.5)
     
         scaled_observations_r = spline_model_r[:auxM][:XWy]
     
@@ -51,71 +62,64 @@ include("ReshapeROutput.jl")
     @testset "R's storage of Tikhonov matrix" begin
         weighted_design_matrix_r = vector_to_banded_matrix(spline_model_r[:auxM][:XWX])
         gram_r = vector_to_banded_matrix(spline_model_r[:auxM][:Sigma])
-    
-        tikhonov_matrix_r = weighted_design_matrix_r + spline_model_r[:lambda] * gram_r
+        tikhonov_matrix_r1 = weighted_design_matrix_r + spline_model_r[:lambda] * gram_r
+
         cholesky_r = vector_to_upper_triangular(spline_model_r[:auxM][:R])
+        tikhonov_matrix_r2 = transpose(cholesky_r) * cholesky_r
         
-        @test transpose(cholesky_r) * cholesky_r ≈ tikhonov_matrix_r
-        @test maximum(abs, transpose(cholesky_r) * cholesky_r - tikhonov_matrix_r) < 1e-14
+        @test tikhonov_matrix_r1 ≈ tikhonov_matrix_r2
+        @test maximum(abs, tikhonov_matrix_r1 - tikhonov_matrix_r2) < 1e-10
     end
     
+
+    @testset "Check computation of Gram matrix entries" begin
+        gram_julia = SmoothSpline.compute_gram_matrix(spline_data)
+
+        gram_quadgk = similar(gram_julia)
+
+        knots = unique(spline_data.B.Knots)
+        max_integration_error = 0
+
+        for i in 1:size(gram_julia, 1)
+            for j in 1:size(gram_julia, 2)
+                gram_quadgk[i, j], integration_error = QuadGK.quadgk(
+                    u -> SmoothSpline.single_basis_function_deriv(i - 1, u, 2, spline_data.B)[2] * 
+                    SmoothSpline.single_basis_function_deriv(j - 1, u, 2, spline_data.B)[2],
+                    knots...
+                )
+
+                max_integration_error = max(integration_error, max_integration_error)
+            end
+        end
+
+        @test gram_quadgk ≈ transpose(gram_quadgk)
+        @test gram_julia ≈ transpose(gram_julia)
+
+        @test max_integration_error <= 1e-8
+
+        @test maximum(abs, gram_julia - gram_quadgk) < 10 * max_integration_error
+    end
+
     
     @testset "Compare Gram matrix with R" begin
         gram_julia = SmoothSpline.compute_gram_matrix(spline_data)
     
-        lambda_r = spline_model_r[:lambda]
         gram_r = vector_to_banded_matrix(spline_model_r[:auxM][:Sigma])
-    
-        # maximum(abs, lambda_r * gram_r - lambda_julia * gram_julia)
+
+        @show maximum(abs, gram_julia - gram_r)
+        # @test maximum(abs, gram_julia - gram_r) < 1e-5
     end
     
     
-    @testset "Compare Tikhonov matrix with R" begin
+    @testset "Julia's Tikhonov matrix is better conditioned than R's" begin
         cholesky_r = vector_to_upper_triangular(spline_model_r[:auxM][:R])
         tikhonov_matrix_r = transpose(cholesky_r) * cholesky_r
     
-        tikhonov_matrix_julia = compute_tikhonov_matrix(spline_data, 0.5)[1]
-    
-        # @test tikhonov_matrix_julia ≈ tikhonov_matrix_r
-        # @test maximum(abs, tikhonov_matrix_julia - tikhonov_matrix_r) < 1e-14
+        tikhonov_matrix_julia, _ = compute_tikhonov_matrix(spline_data, 0.5)
+
+        condition_number_r = LinearAlgebra.cond(tikhonov_matrix_r)
+        condition_number_julia = LinearAlgebra.cond(tikhonov_matrix_julia)
+
+        @test condition_number_julia <= condition_number_r
     end
-
 end
-
-
-
-# R = zeros(size(gram_r))
-# # factorization in R with dpbfa.f. Storage format is peculiar; works backwards
-# R[1, 1] = spline_model_r[:auxM][:R][4]
-# R[1:2, 2] = spline_model_r[:auxM][:R][7:8]
-# R[1:3, 3] = spline_model_r[:auxM][:R][10:12]
-# for col in 4:24
-#     idx1 = col - 4 .+ (1:4)
-#     idx2 = (col - 1) * 4 .+ (1:4)
-#     R[idx1, col] = spline_model_r[:auxM][:R][idx2]
-# end
-
-# lambda = ss_r[:lambda]
-
-# rnm = XWX + lambda * sigma
-
-# # Very badly conditioned rnm
-# cond(rnm)
-# c = cholesky(rnm)
-
-# maximum(abs, c.U - R)
-# maximum(abs, ridge_normal_matrix - rnm)
-# maximum(abs, scaled_y - ss_r[:auxM][:XWy])
-
-# # Like dpbsl.f in R. See also sslvrg.f
-# A = deepcopy(rnm)
-# B = deepcopy(scaled_y)
-# LinearAlgebra.LAPACK.posv!('U', A, B)
-
-# # Alternatively:
-# # # Cholesky of A
-# # LinearAlgebra.LAPACK.potrf!('U', A)
-# # LinearAlgebra.LAPACK.potrs!('U', A, B)
-
-# coef = ss_r[:fit][:coef]
-# maximum(abs, coef - B)
